4c291b60ec898c1bb35cbe2f2d3c0184df52f143
Charless Wesley Arya <wesleyarya40@gmail.com>
2022-08-20 23:54:54 +0530

    added every functionality in it

--- js/script.js
+++ js/script.js
@@ -94,11 +94,24 @@ let words = [
 ];
 const wordText = document.querySelector(".word"),
   hintText = document.querySelector(".hint span"),
+  timeText = document.querySelector(".time b"),
   inputField = document.querySelector("input"),
   refreshBtn = document.querySelector(".refresh-word"),
   checkBtn = document.querySelector(".check-word");
-let correctWord;
+let correctWord, timer;
+const initTimer = (maxTime) => {
+  timer = setInterval(() => {
+    if (maxTime > 0) {
+      maxTime--;
+      return (timeText.innerText = maxTime);
+    }
+    clearInterval(timer);
+    alert(`Time Up ${correctWord.toLocaleUpperCase()} was the  correct word`);
+    initGame(); //calling initGame function, to restart
+  }, 1000);
+};
 const initGame = () => {
+  initTimer(30); //calling initTimer function with 30 seconds as maxTime value
   let randomObj = words[Math.floor(Math.random() * words.length)]; //getting random object from words
   let wordArray = randomObj.word.split(""); //splitting each letter of random word
   for (let i = wordArray.length - 1; i > 0; i--) {
@@ -109,14 +122,20 @@ const initGame = () => {
   wordText.innerText = wordArray.join(""); //passing shuffled word as word text
   hintText.innerText = randomObj.hint; //passing random object hint as hint text
   correctWord = randomObj.word.toLocaleLowerCase(); //passing random word to correct word
+  inputField.value = ""; //making input field empty
+  inputField.setAttribute("maxlength", correctWord.length); //getting input maxlength att value to word length
   console.log(wordArray, randomObj.word);
 };
 initGame();
 
 const checkWord = () => {
   let userWord = inputField.value.toLocaleLowerCase();
+  if (!userWord) return alert("please enter a word check");
   if (userWord !== correctWord) {
     return alert(`Oops! ${userWord} is not a correct word`);
+  } else {
+    alert(`Congrats ${userWord.toLocaleUpperCase()} is a correct word`);
+    initGame();
   }
 };
 refreshBtn.addEventListener("click", initGame);
